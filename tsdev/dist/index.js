ebc572568544628e0c4c09dc7d9099c20790d337
knuddelina <145656660+knuddelina@users.noreply.github.com>
2023-11-30 09:12:35 +0000

    first commit

---
+++ tsdev/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+class Main {
+    static sayHello(n) {
+        for (let i = 0; i < n; i++) {
+            console.log("Moinsen");
+        }
+    }
+}
+Main.sayHello(2);
+//# sourceMappingURL=index.js.map
---
+++ tsdev/src/index.ts
@@ -0,0 +1,9 @@
+class Main{
+    static sayHello(n:number){
+        for(let i=0; i<n; i++){
+            console.log("Moinsen");
+        }
+    }
+}
+
+Main.sayHello(2);
